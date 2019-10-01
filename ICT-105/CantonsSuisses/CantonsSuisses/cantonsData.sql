a9542189bcab60f81d5a188487b5c0931a0cea36
dmitrimeili <47849645+dmitrimeili@users.noreply.github.com>
2019-10-01 16:00:41 +0200

    wip

---
+++ ICT-105/CantonsSuisses/CantonsSuisses/cantonsData.sql
@@ -0,0 +1,38 @@
+/*  
+-- Titre        : cantonsData.sql
+-- Auteur       : Raphaël Favre
+
+-- Created      : 21.09.2019
+
+-- Version      : 1.0
+-- Description  : MySQL Script containing datas for table "cantons" from DB "swissCantons" 
+
+
+*/
+
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('AG','Argovie',1803,'Aarau',662224,1403.73,212,908,260,476);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('AI','Appenzell Rhodes-Intérieures',1513,'Appenzell',16003,172.52,6,2502,560,1126);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('AR','Appenzell Rhodes-Extérieures',1513,'Herizau',54954,242.86,20,2500,440,935);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('BE','Berne',1353,'Berne',1026513,5959.44,347,4274,402,1198);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('BL','Bâle-Campagne',1501,'Liestal',286848,517.56,86,1169,246,521);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('BS','Bâle-Ville',1501,'Bâle',198249,37.00,3,522,245,295);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('FR','Fribourg',1481,'Fribourg',311914,1670.70,136,2389,429,856);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('GE','Genève',1815,'Genève',495325,282.48,45,516,332,419);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('GL','Glaris',1352,'Glaris',40147,685.30,3,3614,410,1589);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('GR','Grisons',1803,'Coire',197550,7105.44,108,4049,260,2021);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('JU','Jura',1979,'Delémont',73122,838.55,55,1302,364,690);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('NE','Neuchâtel',1815,'Neuchâtel',178567,802.93,31,1552,429,919);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('NW','Nidwald',1291,'Stans',42556,275.90,11,2901,434,1077);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('OW','Obwald',1291,'Sarnen',37378,490.59,7,3238,434,1329);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('SA','Sardaigne',2018,'Cagliari',1649412,24090.00,377,1834,NULL,500);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('SG','Saint-Gall',1803,'Saint-Gall',502552,2025.54,77,3248,396,1000);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('SH','Schaffhouse',1501,'Schaffhouse',80769,298.42,26,912,344,538);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('SO','Soleure',1481,'Soleure',269441,790.49,109,1445,277,630);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('SZ','Schwytz',1291,'Schwytz',155863,906.92,30,2802,406,1082);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('TG','Thurgovie',1803,'Frauenfeld',270709,991.02,80,991,370,495);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('TI','Tessin',1803,'Bellinzone',354375,2812.20,115,3402,193,1412);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('UR','Uri',1291,'Altdorf',36145,1076.57,20,3630,434,1896);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('VD','Vaud',1803,'Lausanne',784822,3212.03,309,3210,372,827);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('VS','Valais',1815,'Sion',339176,5224.25,126,4634,372,2140);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('ZG','Zoug',1352,'Zoug',123948,238.69,11,1580,388,651);
+INSERT INTO cantons(code,name,since,capital,population,area,nbTowns,altitudeMax,altMin,altMoy) VALUES ('ZH','Zurich',1351,'Zurich',1487969,1729.00,166,1292,332,533);
